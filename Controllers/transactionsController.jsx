4c1991c49dd95109ae2af39dbe2df01e565d091d
USERNAME <EMAIL>
2023-04-17 23:30:05 -0400

    EOD commit

--- Controllers/transactionsController.jsx
+++ Controllers/transactionsController.jsx
@@ -8,6 +8,12 @@ transactions.get('/', (req, res) => {
     res.json(transactionsArray)
 })
 
+// Delete
+transactions.delete('/:id', (req, res) => {
+    const { id } = req.params
 
+    const deletedTransactions = transactionsArray.splice(id, 1)
+    res.status(202).json(deletedTransactions)  
+})
 
 module.exports = transactions;
