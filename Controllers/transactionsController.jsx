de8cb97ca5e52c9ec446ba5733e8f6a583432e21
USERNAME <EMAIL>
2023-04-18 19:02:24 -0400

    BackEnd complete

--- Controllers/transactionsController.jsx
+++ Controllers/transactionsController.jsx
@@ -11,9 +11,21 @@ transactions.get('/', (req, res) => {
 // Delete
 transactions.delete('/:id', (req, res) => {
     const { id } = req.params
-
     const deletedTransactions = transactionsArray.splice(id, 1)
     res.status(202).json(deletedTransactions)  
 })
+
+// CREATE
+transactions.post("/newtransaction", (req, res) => {
+    const newTransactions = req.body;
+    transactionsArray.push(newTransactions);
+    res.status(201).json({ success: true, payload: newLog });
+  });
+
+ 
+
+
+
+
 
 module.exports = transactions;
