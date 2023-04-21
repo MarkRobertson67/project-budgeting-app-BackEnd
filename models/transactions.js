af32364c4c87616a0e1e4293322e19773967ac2d
USERNAME <EMAIL>
2023-04-21 16:43:07 -0400

    fixed models

--- models/transactions.js
+++ models/transactions.js
@@ -1,6 +1,6 @@
 const transactions = [
     {
-      id: 0,
+      id: "0",
       item_name: 'Salary',
       amount: 1500,
       date: '2022-01-15',
@@ -9,81 +9,81 @@ const transactions = [
       deposit: true
     },
     {
-      id: 1,
+      id: "1",
       item_name: 'Groceries',
-      amount: -150,
+      amount: 150,
       date: '2022-01-10',
       from: 'Walmart',
       category: 'Food',
       deposit: false
     },
     {
-      id: 2,
+      id: "2",
       item_name: 'Gas',
-      amount: -50,
+      amount: 50,
       date: '2022-01-09',
       from: 'Shell',
       category: 'Transportation',
       deposit: false
     },
     {
-      id: 3,
+      id: "3",
       item_name: 'Movie Tickets',
-      amount: -30,
+      amount: 30,
       date: '2022-01-07',
       from: 'AMC Theatres',
       category: 'Entertainment',
       deposit: false
     },
     {
-      id: 4,
+      id: "4",
       item_name: 'Gift for Friend',
-      amount: -50,
+      amount: 50,
       date: '2022-01-05',
       from: 'Amazon',
       category: 'Gifts',
       deposit: false
     },
     {
-      id: 5,
+      id: "5",
       item_name: 'Rent',
-      amount: -1000,
+      amount: 1000,
       date: '2022-01-01',
       from: 'Landlord',
       category: 'Housing',
       deposit: false
     },
     {
-      id: 6,
+      id: "6",
       item_name: 'Internet Bill',
-      amount: -80,
+      amount: 80,
       date: '2021-12-29',
       from: 'Comcast',
       category: 'Utilities',
       deposit: false
     },
     {
-      id: 7,
+      id: "7",
       item_name: 'New Shoes',
-      amount: -100,
+      amount: 100,
       date: '2021-12-25',
       from: 'Nike',
       category: 'Clothing',
       deposit: false
     },
     {
-      id: 8,
+      id: "8",
       item_name: 'Dinner with Friends',
-      amount: -70,
+      amount: 70,
       date: '2021-12-22',
       from: 'Local Restaurant',
       category: 'Food',
       deposit: false
     },
     {
-      id: 9,
+      id: "9",
       item_name: 'Phone Bill',
-      amount: -50,
+      amount: 50,
       date: '2021-12-20',
       from: 'Verizon',
       category: 'Utilities',
